cd0f097310f2b9a9ac7b387183018d4e8da80670
Abdelrahman Adel <gharibaboddahab@gmail.com>
2023-05-05 15:27:59 +0300

    updated

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,9 @@ router.post("/register", async (req, res) => {
   if (user) {
     return res.status(400).json({ message: "Username already exists" });
   }
-  console.log(password);
+  if (!email||!password||!username) {
+    return res.status(400).send("please fill all credentials");
+  }
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new userModel({ username, password: hashedPassword, email });
 
